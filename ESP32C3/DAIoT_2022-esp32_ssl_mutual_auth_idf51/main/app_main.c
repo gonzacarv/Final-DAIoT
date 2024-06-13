860eb6b09ea7c80b87bcd9783b818c7f968abf1a
gonzacarv <gonzacarv@gmail.com>
2024-06-13 18:30:58 -0300

    Entrega final DAIoT

---
+++ ESP32C3/DAIoT_2022-esp32_ssl_mutual_auth_idf51/main/app_main.c
@@ -0,0 +1,194 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stddef.h>
+#include <string.h>
+#include "esp_wifi.h"
+#include "esp_system.h"
+#include "nvs_flash.h"
+#include "esp_event.h"
+#include "esp_netif.h"
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/semphr.h"
+#include "freertos/queue.h"
+
+#include "lwip/sockets.h"
+#include "lwip/dns.h"
+#include "lwip/netdb.h"
+
+#include "esp_log.h"
+#include "mqtt_client.h"
+#include "driver/gpio.h"
+
+#define EXAMPLE_ESP_WIFI_SSID      "" //configurado con "config"
+#define EXAMPLE_ESP_WIFI_PASS      ""
+
+// Set your local broker URI
+#define BROKER_URI "mqtts://192.168.0.172:8883"
+
+// Define the GPIO for the button
+#define BUTTON_PIN GPIO_NUM_9
+
+// Define the consumption ID to monitor
+#define CONSUMPTION_ID "5"
+
+static const char *TAG = "Domotico-DAIoT";
+
+static esp_mqtt_client_handle_t client;
+
+extern const uint8_t client_cert_pem_start[] asm("_binary_client_crt_start");
+extern const uint8_t client_cert_pem_end[] asm("_binary_client_crt_end");
+extern const uint8_t client_key_pem_start[] asm("_binary_client_key_start");
+extern const uint8_t client_key_pem_end[] asm("_binary_client_key_end");
+extern const uint8_t server_cert_pem_start[] asm("_binary_broker_CA_crt_start");
+extern const uint8_t server_cert_pem_end[] asm("_binary_broker_CA_crt_end");
+
+static void log_error_if_nonzero(const char *message, int error_code) {
+    if (error_code != 0) {
+        ESP_LOGE(TAG, "Last error %s: 0x%x", message, error_code);
+    }
+}
+
+static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data) {
+    ESP_LOGD(TAG, "Event dispatched from event loop base=%s, event_id=%ld", base, event_id);
+    esp_mqtt_event_handle_t event = event_data;
+
+    switch ((esp_mqtt_event_id_t)event_id) {
+    case MQTT_EVENT_CONNECTED:
+        ESP_LOGI(TAG, "MQTT_EVENT_CONNECTED");
+        esp_mqtt_client_subscribe(client, "hogar/consumos/" CONSUMPTION_ID "/estado", 0);
+        esp_mqtt_client_subscribe(client, "hogar/consumos/" CONSUMPTION_ID "/intensidad", 0);
+        break;
+
+    case MQTT_EVENT_DISCONNECTED:
+        ESP_LOGI(TAG, "MQTT_EVENT_DISCONNECTED");
+        break;
+
+    case MQTT_EVENT_SUBSCRIBED:
+        ESP_LOGI(TAG, "MQTT_EVENT_SUBSCRIBED, msg_id=%d", event->msg_id);
+        break;
+
+    case MQTT_EVENT_UNSUBSCRIBED:
+        ESP_LOGI(TAG, "MQTT_EVENT_UNSUBSCRIBED, msg_id=%d", event->msg_id);
+        break;
+
+    case MQTT_EVENT_PUBLISHED:
+        ESP_LOGI(TAG, "MQTT_EVENT_PUBLISHED, msg_id=%d", event->msg_id);
+        break;
+
+    case MQTT_EVENT_DATA:
+        ESP_LOGI(TAG, "MQTT_EVENT_DATA");
+        printf("TOPIC=%.*s\r\n", event->topic_len, event->topic);
+        printf("DATA=%.*s\r\n", event->data_len, event->data);
+
+        if (strncmp(event->topic, "hogar/consumos/" CONSUMPTION_ID "/estado", event->topic_len) == 0) {
+            if (strncmp(event->data, "true", event->data_len) == 0) {
+                ESP_LOGI(TAG, "STATUS = ON");
+            } else if (strncmp(event->data, "false", event->data_len) == 0) {
+                ESP_LOGI(TAG, "STATUS = OFF");
+            }
+        } else if (strncmp(event->topic, "hogar/consumos/" CONSUMPTION_ID "/intensidad", event->topic_len) == 0) {
+            ESP_LOGI(TAG, "INT = %.*s", event->data_len, event->data);
+        }
+
+        break;
+
+    case MQTT_EVENT_ERROR:
+        ESP_LOGI(TAG, "MQTT_EVENT_ERROR");
+        if (event->error_handle->error_type == MQTT_ERROR_TYPE_TCP_TRANSPORT) {
+            log_error_if_nonzero("reported from esp-tls", event->error_handle->esp_tls_last_esp_err);
+            log_error_if_nonzero("reported from tls stack", event->error_handle->esp_tls_stack_err);
+            log_error_if_nonzero("captured as transport's socket errno", event->error_handle->esp_transport_sock_errno);
+            ESP_LOGI(TAG, "Last errno string (%s)", strerror(event->error_handle->esp_transport_sock_errno));
+        }
+        break;
+
+    default:
+        ESP_LOGI(TAG, "Other event id:%d", event->event_id);
+        break;
+    }
+}
+
+static void mqtt_app_start(void) {
+    const esp_mqtt_client_config_t mqtt_cfg = {
+        .broker.address.uri = BROKER_URI,
+        .broker.verification.certificate = (const char *)server_cert_pem_start,
+        .credentials.authentication.certificate = (const char *)client_cert_pem_start,
+        .credentials.authentication.key = (const char *)client_key_pem_start,
+    };
+
+    ESP_LOGI(TAG, "[APP] Free memory: %ld bytes", esp_get_free_heap_size());
+    client = esp_mqtt_client_init(&mqtt_cfg);
+    esp_mqtt_client_register_event(client, ESP_EVENT_ANY_ID, mqtt_event_handler, NULL);
+    esp_mqtt_client_start(client);
+}
+
+static void button_task(void *arg) {
+    bool estado = false;
+    while (1) {
+        if (gpio_get_level(BUTTON_PIN) == 0) {
+            vTaskDelay(50 / portTICK_PERIOD_MS);  // Debounce delay
+            if (gpio_get_level(BUTTON_PIN) == 0) {
+                estado = !estado;
+                const char *estado_str = estado ? "true" : "false";
+                esp_mqtt_client_publish(client, "hogar/consumos/" CONSUMPTION_ID "/estado", estado_str, 0, 1, 0);
+                ESP_LOGI(TAG, "Button pressed, estado toggled to %s", estado_str);
+                while (gpio_get_level(BUTTON_PIN) == 0) {
+                    vTaskDelay(10 / portTICK_PERIOD_MS);  // Wait for button release
+                }
+            }
+        }
+        vTaskDelay(10 / portTICK_PERIOD_MS);  // Polling delay
+    }
+}
+
+void wifi_init_sta(void) {
+    esp_netif_init();
+    esp_event_loop_create_default();
+    esp_netif_create_default_wifi_sta();
+
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    esp_wifi_init(&cfg);
+
+    wifi_config_t wifi_config = {
+        .sta = {
+            .ssid = EXAMPLE_ESP_WIFI_SSID,
+            .password = EXAMPLE_ESP_WIFI_PASS,
+            .threshold.authmode = WIFI_AUTH_WPA2_PSK,
+        },
+    };
+
+    esp_wifi_set_mode(WIFI_MODE_STA);
+    esp_wifi_set_config(WIFI_IF_STA, &wifi_config);
+    esp_wifi_start();
+
+    ESP_LOGI(TAG, "wifi_init_sta finished.");
+
+    ESP_ERROR_CHECK(esp_wifi_connect());
+}
+
+void app_main(void) {
+    ESP_LOGI(TAG, "[APP] Startup..");
+    ESP_LOGI(TAG, "[APP] Free memory: %ld bytes", esp_get_free_heap_size());
+    ESP_LOGI(TAG, "[APP] IDF version: %s", esp_get_idf_version());
+
+    esp_log_level_set("*", ESP_LOG_INFO);
+    esp_log_level_set("MQTT_CLIENT", ESP_LOG_VERBOSE);
+    esp_log_level_set("TRANSPORT_BASE", ESP_LOG_VERBOSE);
+    esp_log_level_set("TRANSPORT", ESP_LOG_VERBOSE);
+    esp_log_level_set("OUTBOX", ESP_LOG_VERBOSE);
+
+    ESP_ERROR_CHECK(nvs_flash_init());
+
+    wifi_init_sta();
+    mqtt_app_start();
+
+    // Configure button
+    gpio_reset_pin(BUTTON_PIN);
+    gpio_set_direction(BUTTON_PIN, GPIO_MODE_INPUT);
+    gpio_set_pull_mode(BUTTON_PIN, GPIO_PULLUP_ONLY);
+
+    xTaskCreate(button_task, "button_task", 2048, NULL, 10, NULL);
+}
+
